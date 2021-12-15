84598e0bb00595813de3799ac40f1aa1ae768586
Jake Grainger <j.p.grainger2@outlook.com>
2021-12-15 17:06:40 +0000

    Updated docstring.

--- src/fit.jl
+++ src/fit.jl
@@ -1,11 +1,13 @@
 """
     fit(ts,Δ;model::Type{<:TimeSeriesModel},x₀,lowerΩcutoff,upperΩcutoff,x_lowerbounds,x_upperbounds,method,taper)
+    fit(timeseries::TimeSeries;model::Type{<:TimeSeriesModel},x₀,lowerΩcutoff,upperΩcutoff,x_lowerbounds,x_upperbounds,method,taper)
 
 Fit a wave model using `IPNewton` method from Optim.jl.
 
 # Arguments
 - `ts`: `n` by `D` matrix containing the timeseries (or vector if `D=1`), where `n` is the number of observations and `D` is the number of series.
 - `Δ`: The sampling rate, which should be a positive real number.
+- `timeseries`: Can be provided in place of `ts` and `Δ`.
 - `model`: The model which will be fitted. Should be a type (not a realisation of the model) e.g. JONSWAP{k} not JONSWAP{K}(x).
 - `x₀`: The initial parameter guess.
 - `lowerΩcutoff`: The lower cutoff for the frequency range to be used in fitting. Default is `0`.
