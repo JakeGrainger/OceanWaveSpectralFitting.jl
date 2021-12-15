eef3f763a1de53e3bdde24dd4c53de8444480e72
Jake Grainger <j.p.grainger2@outlook.com>
2021-12-15 11:22:48 +0000

    Added Hessian.

--- test/models/multivariate/JS_BWG_HNE_test.jl
+++ test/models/multivariate/JS_BWG_HNE_test.jl
@@ -19,9 +19,10 @@
         @test approx_gradient(θ -> sdf(JS_BWG_HNE{1}(θ), ω), θ₀)      ≈ grad_sdf(JS_BWG_HNE{1}(θ₀), ω)
         @test approx_gradient(θ -> sdf(JS_BWG_HNE{1}(θ), 0), θ₀)      ≈ grad_sdf(JS_BWG_HNE{1}(θ₀), 0)
     end
-    # @testset "Hessian" begin
-    #     @test approx_hessian( θ -> grad_sdf(JS_BWG_HNE{1}(θ), ω), θ₀) ≈ hess_sdf(JS_BWG_HNE{1}(θ₀), ω)
-    # end
+    @testset "Hessian" begin
+        @test approx_hessian( θ -> grad_sdf(JS_BWG_HNE{1}(θ), ω), θ₀) ≈ hess_sdf(JS_BWG_HNE{1}(θ₀), ω)
+        @test approx_hessian( θ -> grad_sdf(JS_BWG_HNE{1}(θ), 0), θ₀) ≈ hess_sdf(JS_BWG_HNE{1}(θ₀), 0)
+    end
     @testset "Error handling" begin
         @test_throws ArgumentError JS_BWG_HNE{1}(-0.7,0.9, 3.3, 5.0, 0, 4, 2.7, 0.55, 0.26)
         @test_throws ArgumentError JS_BWG_HNE{1}(0.7,-0.9, 3.3, 5.0, 0, 4, 2.7, 0.55, 0.26)
