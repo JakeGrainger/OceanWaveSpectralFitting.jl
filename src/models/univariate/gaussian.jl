d244a158db9bd9aa8d180bb5adb0a8d7f8f3e0de
Jake Grainger <j.p.grainger2@outlook.com>
2022-02-24 15:21:29 +0000

    Added univariate Gaussian sdf model.

---
+++ src/models/univariate/gaussian.jl
@@ -0,0 +1,48 @@
+struct Gaussian{K} <: WhittleLikelihoodInference.UnknownAcvTimeSeriesModel{3,Float64}
+    α::Float64
+    ωₚ::Float64
+    σ::Float64
+
+    norm::Float64
+    halfσ⁻²::Float64
+    function Gaussian{K}(α,ωₚ,σ) where {K}
+        α > 0 || throw(ArgumentError("α must be > 0."))
+        ωₚ > 0 || throw(ArgumentError("ωₚ must be > 0."))
+        σ > 0 || throw(ArgumentError("σ must be > 0."))
+        new(α,ωₚ,σ,α/(σ*sqrt(2π)),inv(2σ^2))
+    end
+    function Gaussian{K}(x) where {K}
+        @boundscheck checkparameterlength(x,Gaussian{K})
+        @inbounds Gaussian{K}(x[1],x[2],x[3])
+    end
+end
+
+Base.@propagate_inbounds @fastmath function WhittleLikelihoodInference.add_sdf!(out, model::Gaussian, ω::Real)
+    @inbounds begin
+        ω = abs(ω)
+        return norm*exp(-(model.ωₚ-ω)^2*model.halfσ⁻²)
+    end
+    nothing
+end
+
+WhittleLikelihoodInference.npars(::Type{Gaussian{K}}) where {K} = 3
+WhittleLikelihoodInference.nalias(::Gaussian{K}) where {K} = K
+
+OceanWaveSpectralFitting.lowerbounds(::Type{Gaussian{K}}) where {K} = [0,  0, 0]
+OceanWaveSpectralFitting.upperbounds(::Type{Gaussian{K}}) where {K} = [Inf,Inf,Inf]
+
+Gaussian(x::AbstractVector{Float64}) = GeneralJONSWAP(1,1,1,1,1)
+Gaussian(α,ωₚ,γ,r,s) = error("GeneralJONSWAP process requires the ammount of aliasing specified as a type parameter. Use GeneralJONSWAP{K}() where K ∈ N.")
+
+Base.@propagate_inbounds @fastmath function WhittleLikelihoodInference.grad_add_sdf!(out, model::Gaussian, ω::Real)
+    @boundscheck checkbounds(out,1:3)
+    @inbounds begin
+        ω = abs(ω)
+        sdf = exp(-(model.ωₚ-ω)^2 * model.halfσ⁻²)
+        out[1] += sdf/α
+        out[2] -= 2(ω-model.ωₚ) * model.halfσ⁻² * sdf
+        out[3] += 2(model.ωₚ-ω)^2 * model.halfσ⁻² * sdf
+        out[3] += sdf * ((ω-ωₚ)^2/model.σ^3-1/model.σ)
+    end
+    nothing
+end
---
+++ test/models/univariate/gaussian_test.jl
@@ -0,0 +1,18 @@
+@testset "Gaussian" begin
+    θ₀ = [0.7, 0.9, 0.1]
+    ω = 1.0
+    
+    @testset "Gradient" begin
+        @test approx_gradient_uni(θ -> sdf(Gaussian{1}(θ), ω), θ₀)      ≈ grad_sdf(Gaussian{1}(θ₀), ω)
+    end
+
+    @testset "Error handling" begin
+        @test_throws ArgumentError Gaussian{1}(-0.7, 0.9, 0.1)
+        @test_throws ArgumentError Gaussian{1}(0.7, -0.9, 0.1)
+        @test_throws ArgumentError Gaussian{1}(0.7, 0.9, -0.1)
+        @test_throws ArgumentError Gaussian{1}(ones(2))
+        @test_throws ArgumentError Gaussian{1}(ones(4))
+        @test_throws ErrorException Gaussian(ones(4))
+        @test_throws ErrorException Gaussian(0.7, 0.9, 0.1)
+    end
+end
